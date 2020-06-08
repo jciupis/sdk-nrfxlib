3434ea9d2661d3957386e8271cf9b253fc28b46f
Ciupis, Jedrzej <jedrzej.ciupis@nordicsemi.no>
2020-06-08 10:53:32 +0200

    nrf_802154_sl: Remove nRF 802.15.4 SL
    This patch removes nRF 802.15.4 SL
    
    Signed-off-by: Ciupis, Jedrzej <jedrzej.ciupis@nordicsemi.no>

--- nrf_802154_sl/include/nrf_802154_sl_stats.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2020 Nordic Semiconductor ASA. All Rights Reserved.
- *
- * The information contained herein is confidential property of Nordic Semiconductor ASA.
- * The use, copying, transfer or disclosure of such information is prohibited except by
- * express written agreement with Nordic Semiconductor ASA.
- */
-
-#define nrf_802154_sl_stat_counter_increment(...)
